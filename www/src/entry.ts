b4f88c104802c3ce17a93e3ccceedd87c22dadda
Arata Furukawa <info@ornew.net>
2016-11-25 08:41:31 +0900

    Improve: Introduced Webpack.

--- www/src/entry.ts
+++ www/src/entry.ts
@@ -1,20 +1,27 @@
-var context;
-var canvas;
+import * as ons from 'onsenui';
+
+const ons_css = require('../node_modules/onsenui/css/onsenui.css');
+const ons_cmp_css = require('../node_modules/onsenui/css/onsen-css-components.css');
+const main_page = require('layout/main_page.html');
+const main_style = require('style/main.scss');
+
+var context: any;
+var canvas: any;
 var mf = false;
 var ox = 0;
 var oy = 0;
 var x = 0;
 var y = 0;
-var step = 100;
+var step = 1000;
 
-function onDown(event){
+function onDown(event: any){
   mf=true;
   ox=event.touches[0].pageX-event.target.getBoundingClientRect().left;
   oy=event.touches[0].pageY-event.target.getBoundingClientRect().top;
   event.stopPropagation();
 }
 
-function onMove(event){
+function onMove(event: any){
   if(mf){
     x=event.touches[0].pageX-event.target.getBoundingClientRect().left;
     y=event.touches[0].pageY-event.target.getBoundingClientRect().top;
@@ -26,18 +33,18 @@ function onMove(event){
   }
 }
 
-function onUp(event){
+function onUp(event: any){
   mf=false;
   event.stopPropagation();
 }
 
-function onMouseDown(event){
+function onMouseDown(event: any){
   ox=event.clientX-event.target.getBoundingClientRect().left;
   oy=event.clientY-event.target.getBoundingClientRect().top ;
   mf=true;
 }
 
-function onMouseMove(event){
+function onMouseMove(event: any){
   if(mf){
     x=event.clientX-event.target.getBoundingClientRect().left;
     y=event.clientY-event.target.getBoundingClientRect().top ;
@@ -47,7 +54,7 @@ function onMouseMove(event){
   }
 }
 
-function onMouseUp(event){
+function onMouseUp(event: any){
   mf=false;
 }
 
@@ -65,7 +72,7 @@ function clearCanvas(){
 
 function getPixels(){
   var pixels = context.getImageData(0, 0, 28, 28).data;
-  var result = [];
+  var result: number[] = [];
   for(var y = 0; y < 28; ++y) {
     for(var x = 0; x < 28; ++x) {
       var i = (y * 28 + x) * 4;
@@ -75,13 +82,13 @@ function getPixels(){
   return result;
 }
 
-function print(m) {
+function print(m: any) {
   var log = document.createElement('p');
   log.innerHTML = m;
   document.getElementById("log").appendChild(log);
 }
 
-function error(m) {
+function error(m: any) {
   var now = new Date();
   var log = document.createElement('p');
   log.className = 'error';
@@ -94,14 +101,14 @@ function evalute() {
   xhttp.onreadystatechange = function() {
     if (this.readyState == 4 && this.status == 200) {
       console.log(this.responseText);
-      response = JSON.parse(this.responseText);
+      var response = JSON.parse(this.responseText);
       if('error' in response) {
         error(response['error']);
         return;
       }
       document.getElementById("inference").innerHTML = response['inference'];
       for(var i = 0; i < 10; ++i) {
-        document.getElementById("inference-" + i).innerHTML = Math.round(response['results'][i]);
+        document.getElementById("inference-" + i).innerHTML = String(Math.round(response['results'][i]));
       }
     }
   };
@@ -109,11 +116,22 @@ function evalute() {
   xhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
   xhttp.send("payload=" + JSON.stringify({
     'data': getPixels(),
-    'step': parseInt(step)
+    'step': step
   }));
 }
 
 window.onload = function(){
+  document.body.innerHTML = main_page;
+  if(ons.platform.isIOS()){
+    ons.platform.select('ios');
+    error('ios');
+  } else if(ons.platform.isAndroid()){
+    ons.platform.select('android');
+    error('android');
+  } else {
+    ons.platform.select('android');
+    error('other');
+  }
   canvas = document.getElementById("canvas");
   canvas.addEventListener("touchstart",onDown,false);
   canvas.addEventListener("touchmove",onMove,false);
@@ -124,10 +142,10 @@ window.onload = function(){
   canvas.setAttribute("width", 28);
   canvas.setAttribute("height", 28);
 
-  clear_button = document.getElementById("clear");
+  let clear_button = document.getElementById("clear");
   clear_button.addEventListener("click", clearCanvas);
 
-  evalute_button = document.getElementById("evalute");
+  let evalute_button = document.getElementById("evalute");
   evalute_button.addEventListener("click", evalute);
 
   context = canvas.getContext( "2d" );
@@ -138,13 +156,12 @@ window.onload = function(){
   context.lineCap   = "round";
   clearCanvas();
 
+  let step_seekbar = <HTMLInputElement> document.getElementById("step_seekbar");
+  let step_display = document.getElementById("step_display");
   function onSeek(){
-    step = step_seekbar.value;
-    step_display.innerHTML = step;
+    step = parseInt(step_seekbar.value);
+    step_display.innerHTML = step_seekbar.value;
   }
-  step_seekbar = document.getElementById("step_seekbar");
-  step_display = document.getElementById("step_display");
   step_seekbar.addEventListener("input", onSeek);
-  onSeek();
 }
 
---
+++ www/webpack.config.js
@@ -0,0 +1,44 @@
+var path = require("path");
+module.exports = {
+  entry: {
+    app: ["./src/entry.ts"]
+  },
+  output: {
+    path: path.resolve(__dirname, "../root"),
+    filename: "mnist-visualize-example.js",
+    chunkFilename: '[chunkhash].js'
+  },
+  module: {
+    loaders: [{
+      test: /\.(scss|css)/,
+      loaders: ["style", "css", "sass"]
+    }, {
+      test: /\.ts/,
+      loaders: ["ts"]
+    }, {
+      test   : /\.(png|jpg|gif)$/,
+      loader : 'url?limit=8192'
+    }, {
+      test   : /\.(html|htm)$/,
+      loader : 'html'
+    }, {
+      test   : /\.(ttf|eot|svg|woff(2)?)(\?[a-z0-9=&.]+)?$/,
+      loader : 'file?name=res/[name].[ext]'
+    }]
+  },
+  resolve: {
+    root: path.resolve(__dirname, "./src"),
+    extensions: ['', '.webpack.js', '.web.js', '.ts', '.tsx', '.js', '.html']
+  },
+  devServer: {
+    contentBase: "./public",
+  },
+  htmlLoader: {
+    ignoreCustomFragments: [/\{\{.*?}}/],
+    root: path.resolve(__dirname, 'src'),
+    attrs: ['img:src', 'link:href']
+  },
+  externals:[
+    require('webpack-require-http')
+  ]
+};
