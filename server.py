e6a951a57d91bfcdf17fcbfddb86fdefd9b7ee44
Arata Furukawa <info@ornew.net>
2016-11-25 05:16:16 +0900

    Improve: Add server options

--- server.py
+++ server.py
@@ -1,14 +1,34 @@
 #!/usr/bin/env python
 
+import os
+import argparse
 import BaseHTTPServer
 import CGIHTTPServer
 import cgitb; cgitb.enable()  ## This line enables CGI error reporting
 
-server = BaseHTTPServer.HTTPServer
-handler = CGIHTTPServer.CGIHTTPRequestHandler
-#handler.cgi_directories = ["."]
-server_address = ("", 59630)
+FLAGS = None
 
-httpd = server(server_address, handler)
-httpd.serve_forever()
+def run_server():
+  os.chdir(FLAGS.root_dir)
+  server = BaseHTTPServer.HTTPServer
+  handler = CGIHTTPServer.CGIHTTPRequestHandler
+  server_address = ("", FLAGS.port)
+  httpd = server(server_address, handler)
+  httpd.serve_forever()
+
+if __name__ == '__main__':
+  parser = argparse.ArgumentParser()
+  parser.add_argument(
+      '-p',
+      '--port',
+      type=int,
+      default=8000,
+      help='Port number.')
+  parser.add_argument(
+      '--root_dir',
+      type=str,
+      default=os.path.join(os.getcwd(), 'www'),
+      help='Server root directory.')
+  FLAGS, unparsed = parser.parse_known_args()
+  run_server()
 
