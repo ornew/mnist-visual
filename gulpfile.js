1e6d03ae9801be44bc332abe6b1fc6baa119cce3
Arata Furukawa <info@ornew.net>
2016-11-30 03:22:49 +0900

    Improve: add deploy command.

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,12 @@
 var gulp = require('gulp');
+var zip = require('gulp-zip');
+var fs = require('fs');
 var seq = require('run-sequence');
 var del = require('del');
 var webpack = require('webpack-stream');
 var webpack_config = require('./webpack.config.js');
+
+var config = JSON.parse(fs.readFileSync('./package.json'));
 
 gulp.task('compile', function () {
   return gulp.src('./src/app/entry.ts')
@@ -26,4 +30,12 @@ gulp.task('build', ['copy', 'compile']);
 gulp.task('rebuild', function() {
   return seq('clean', 'build');
 });
+gulp.task('deploy:zip', function() {
+  return gulp.src('./build/**/*', { base: './build/' })
+    .pipe(zip(config.name + '_' +config.version + '.zip'))
+    .pipe(gulp.dest('.'))
+})
+gulp.task('deploy', function(){
+  return seq('clean', 'build', 'deploy:zip');
+})
 
